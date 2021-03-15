ebc871eb2f1eb85607c1ab4bd49bc18b08f896aa
linek150 <linek11@vp.pl>
2021-03-15 19:59:23 +0100

    Testbech finished

--- Heuristics/Testbench.py
+++ Heuristics/Testbench.py
@@ -4,14 +4,64 @@ import NearestNeighbor as nn
 import GreedyCycle as gc
 import RegretCycle as rc
 import numpy as np
+import random 
+import copy 
+GC="greedy_cycle"
+RC="regret_cycle"
+NN="nearest_neighbor"
+BEST=0
+WORST=1
+AVG=2
+CYCLES=3
+def get_total_distance(cycle,distance_matrix):
+    total_distance=0
+    for i in range(len(cycle)):
+        total_distance+=distance_matrix[cycle[i-1],cycle[i]]
+    return total_distance
+def get_results(instance_filename):
+    coordinates_a = cdm.load_data_from_file(instance_filename)
+    distance_matrix = cdm.create_distance_matrix(instance_filename)
+    #method_dict [best_distance,worst_distance,avg_distamce,best_cycles]
+    method_dict={i:[np.inf,-np.inf,0,[]] for i in [GC,RC,NN]}
+    for start_vertex in range(len(distance_matrix[0])):
+        #run every method with start_vertex
+        method_dict.get(GC).append(gc.greedy_cycle(distance_matrix,start_vertex))
+        method_dict.get(RC).append(rc.greedy_cycle(distance_matrix,start_vertex))
+        distance_matrix_cp=copy.deepcopy(distance_matrix)
+        method_dict.get(NN).append(nn.nearest_neighbor_method(distance_matrix_cp,start_vertex))
+        #save results
+        for key,method in method_dict.items():
+            total_distance=0
+            #calculate total distance for given method and start_vertex
+            for cycle in method[-1]:
+                cycle_distance=get_total_distance(cycle,distance_matrix)
+                total_distance=total_distance+cycle_distance
+            method[AVG]+=total_distance
+            if total_distance>method[WORST]:
+                method[WORST]=total_distance
+            if total_distance<method[BEST]:
+                method.pop(CYCLES)
+                method[BEST]=total_distance
+                continue
+            #remove iteration cycle
+            method.pop()
+    for key,method in method_dict.items():
+        method[AVG]=method[AVG]/len(distance_matrix[0])
+    return method_dict
+def print_best(method_dict,file_name):
+    coordinates=cdm.load_data_from_file(file_name)
+    xs = [coord[0] for coord in coordinates]
+    ys = [coord[1] for coord in coordinates]
+    print(file_name,": ")
+    for key,method in method_dict.items():
+        vis.plot_results(xs,ys,method[CYCLES][0],method[CYCLES][1])
+        print(key,"avg: ",method[AVG],"best: ", method[BEST],"worst: ",method[WORST])
 
-coordinates_a = cdm.load_data_from_file('kroA100.tsp')
-distance_matrix_a = cdm.create_distance_matrix('kroA100.tsp')
-gc_1, gc_2 = gc.greedy_cycle(distance_matrix_a)
-rc_1, rc_2 =  rc.greedy_cycle(distance_matrix_a)
-nn_1, nn_2 = nn.nearest_neighbor_method(distance_matrix_a)
-xs = [coord[0] for coord in coordinates_a]
-ys = [coord[1] for coord in coordinates_a]
-vis.plot_results(xs, ys, nn_1, nn_2)
-vis.plot_results(xs,ys,gc_1,gc_2)
-vis.plot_results(xs,ys,rc_1,rc_2)
+#method_dict_x method:[best_distance,worst_distance,avg_distamce,best_cycles]
+
+
+
+method_dict_a=get_results("kroA100.tsp")
+print_best(method_dict_a,"kroA100.tsp")
+method_dict_b=get_results("kroB100.tsp")
+print_best(method_dict_b,"kroB100.tsp")
