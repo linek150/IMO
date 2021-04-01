151a5c495d25981f500408050fa19d45c009ca33
Piotr <linek11@vp.pl>
2021-04-01 19:41:34 +0200

    GreedyLS exchange between cycles tested

--- LocalSearch/GreedyLS.py
+++ LocalSearch/GreedyLS.py
@@ -16,7 +16,7 @@ def chain_link_lenght(dist_m,mid,neighbours,cyc):
     to_next=dist_m[cyc[mid],cyc[neighbours[NEXT]]]
     return to_next+to_prev
 def get_delta(dist_m,cyc_idxs,move,cyc,cyc2=None):
-    if cyc2==None:
+    if cyc2 is None:
         curr_distance=0
         new_distance=0
         neighbours=[]
@@ -34,13 +34,21 @@ def get_delta(dist_m,cyc_idxs,move,cyc,cyc2=None):
             hi_idx=cyc_idxs[0] if cyc_idxs[0]>cyc_idxs[1] else cyc_idxs[1]
             curr_distance=dist_m[cyc[low_idx],cyc[low_idx-1]]+dist_m[cyc[hi_idx],cyc[(hi_idx+1)%len(cyc)]]
             new_distance=dist_m[cyc[low_idx-1],cyc[hi_idx]]+dist_m[cyc[low_idx],cyc[(hi_idx+1)%len(cyc)]]
+    else:
+        curr_d_1=dist_m[cyc[cyc_idxs[0]],cyc[cyc_idxs[0]-1]]+dist_m[cyc[cyc_idxs[0]],cyc[(cyc_idxs[0]+1)%len(cyc)]]
+        curr_d_2=dist_m[cyc2[cyc_idxs[1]],cyc2[cyc_idxs[1]-1]]+dist_m[cyc2[cyc_idxs[1]],cyc2[(cyc_idxs[1]+1)%len(cyc)]]
+        curr_distance=curr_d_1+curr_d_2
+        new_d_1=dist_m[cyc2[cyc_idxs[1]],cyc[cyc_idxs[0]-1]]+dist_m[cyc2[cyc_idxs[1]],cyc[(cyc_idxs[0]+1)%len(cyc)]]
+        new_d_2=dist_m[cyc[cyc_idxs[0]],cyc2[cyc_idxs[1]-1]]+dist_m[cyc[cyc_idxs[0]],cyc2[(cyc_idxs[1]+1)%len(cyc)]]
+        new_distance=new_d_1+new_d_2
     return new_distance-curr_distance
 def try_to_improve(dist_m,cyc,cyc2=None):    
     move=EDGES if  rnd.getrandbits(1)   else VERTECIES
     improved=False
-    if cyc2==None:
+    delta=0
+    idxs=np.arange(len(cyc))
+    if cyc2 is None:
         for _ in range(2):
-            idxs=np.arange(len(cyc))
             np.random.shuffle(idxs)
             for i,first_idx in enumerate(idxs):
                 improved=False
@@ -57,6 +65,19 @@ def try_to_improve(dist_m,cyc,cyc2=None):
                 if improved: break
             if improved: break
             move=not(move)
+    else:
+        np.random.shuffle(idxs)
+        idxs2=np.copy(idxs)
+        np.random.shuffle(idxs2)
+        for first_idx in idxs:
+            for sec_idx in idxs2:
+                delta=get_delta(dist_m,(first_idx,sec_idx),move,cyc,cyc2)
+                if delta<0:
+                    improved=True
+                    cyc[first_idx],cyc2[sec_idx]=cyc2[sec_idx],cyc[first_idx]
+                    break
+            if improved:break
+
     return delta,improved,cyc
 def cycle_length(cycle, distance_matrix):
     length = 0
@@ -72,14 +93,16 @@ def cycle_length(cycle, distance_matrix):
 
 def greedy_ls(cyc1,cyc2,dist_m,method=IN):
     imp_cycs=[np.copy(cyc1),np.copy(cyc2)]
-    if method==IN:
-        improved=True
-        liczba_iteracji=0
-        while improved:
+    improved=True
+    liczba_iteracji=0
+    while improved and liczba_iteracji<40:
+        if method==IN:
             for idx,curr_cyc in enumerate(imp_cycs):
-                delta,improved,imp_cycs[idx]=try_to_improve(dist_m,curr_cyc)
-            liczba_iteracji+=1
-        print(liczba_iteracji)
+                _,improved,imp_cycs[idx]=try_to_improve(dist_m,curr_cyc)
+        if method==OUT:
+            delta,improved,_=try_to_improve(dist_m,imp_cycs[0],imp_cycs[1])
+            print(delta)
+        liczba_iteracji+=1
     return imp_cycs
 
 # %%
