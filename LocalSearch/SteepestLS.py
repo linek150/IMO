2159a45b1a941bb8222b1db4001d497d96b886d4
Piotr <linek11@vp.pl>
2021-05-06 11:12:08 +0200

    SteepestLS modified to use delta

--- LocalSearch/SteepestLS.py
+++ LocalSearch/SteepestLS.py
@@ -1,147 +1,92 @@
-import copy
+
+#%%
 import numpy as np
-# from Heuristics.GreedyCycle import greedy_cycle
-# from Heuristics.CreateDistanceMatrix import *
-# from Heuristics.Visualize import plot_results
-# from Heuristics.NearestNeighbor import nearest_neighbor_method
+import random as rnd
+#method 0 - vertecies change
+#method 1 - edges change
+VERTECIES=0
+EDGES=1
+PREV=0
+NEXT=1
+IN=0
+OUT=1
 
+def chain_link_lenght(dist_m,mid,neighbours,cyc):
+    to_prev=dist_m[cyc[mid],cyc[neighbours[PREV]]]
+    to_next=dist_m[cyc[mid],cyc[neighbours[NEXT]]]
+    return to_next+to_prev
+def make_improvement(dist_m,first_idx,sec_idx,cyc,cyc2=None):
+    if cyc2 is None:
+        low_idx, hi_idx= (first_idx,sec_idx) if first_idx<sec_idx else (sec_idx,first_idx)
+        cyc=np.concatenate([cyc[:low_idx],np.flip(cyc[low_idx:hi_idx+1]),cyc[hi_idx+1:]])
+        return cyc
+    else:
+        pass
+    return cyc,cyc2
 
-def cycle_length(cycle, distance_matrix):
-    length = 0
-    lengths = []
-    for idx in range(len(cycle)):
-        if idx - 1 < 0:
-            length += distance_matrix[cycle[-1]][cycle[idx]]
-            lengths.append(distance_matrix[cycle[-1]][cycle[idx]])
-        else:
-            length += distance_matrix[cycle[idx-1]][cycle[idx]]
-            lengths.append(distance_matrix[cycle[idx-1]][cycle[idx]])
-    return length
+def get_delta(dist_m,cyc_idxs,cyc,cyc2=None):
+    curr_distance=-1
+    new_distance=-1
+    if cyc2 is None:
+        if cyc_idxs==(len(cyc)-1,0) or cyc_idxs==(0,len(cyc)-1): return 0
+        low_idx=cyc_idxs[0] if cyc_idxs[0]<cyc_idxs[1] else cyc_idxs[1]
+        hi_idx=cyc_idxs[0] if cyc_idxs[0]>cyc_idxs[1] else cyc_idxs[1]
+        curr_distance=dist_m[cyc[low_idx],cyc[low_idx-1]]+dist_m[cyc[hi_idx],cyc[(hi_idx+1)%len(cyc)]]
+        new_distance=dist_m[cyc[low_idx-1],cyc[hi_idx]]+dist_m[cyc[low_idx],cyc[(hi_idx+1)%len(cyc)]]
+    else:
+        curr_d_1=dist_m[cyc[cyc_idxs[0]],cyc[cyc_idxs[0]-1]]+dist_m[cyc[cyc_idxs[0]],cyc[(cyc_idxs[0]+1)%len(cyc)]]
+        curr_d_2=dist_m[cyc2[cyc_idxs[1]],cyc2[cyc_idxs[1]-1]]+dist_m[cyc2[cyc_idxs[1]],cyc2[(cyc_idxs[1]+1)%len(cyc)]]
+        curr_distance=curr_d_1+curr_d_2
+        new_d_1=dist_m[cyc2[cyc_idxs[1]],cyc[cyc_idxs[0]-1]]+dist_m[cyc2[cyc_idxs[1]],cyc[(cyc_idxs[0]+1)%len(cyc)]]
+        new_d_2=dist_m[cyc[cyc_idxs[0]],cyc2[cyc_idxs[1]-1]]+dist_m[cyc[cyc_idxs[0]],cyc2[(cyc_idxs[1]+1)%len(cyc)]]
+        new_distance=new_d_1+new_d_2
+    return new_distance-curr_distance
+def try_to_improve(cyc1,cyc2,dist_m):   
+    improved=False
+    best_delta=0
+    best_method=-1
+    delta=0
+    idxs=np.arange(len(cyc1))
+    cycs=[cyc1,cyc2]
+    #try to improve both cycle
+    for curr_cyc in range(len(cycs)):
+        for first_idx in idxs:
+            for sec_idx in idxs[first_idx+1:] if first_idx+1<len(cycs[curr_cyc]) else []:
+                delta=get_delta(dist_m,(first_idx,sec_idx),cycs[curr_cyc])
+                if delta<best_delta:
+                   # cycs[curr_cyc]=make_improvement(dist_m,first_idx,sec_idx,cycs[curr_cyc])
+                    #cyc1,cyc2=cycs
+                    best_method=IN
+                    best_delta=delta
+                    best_first=first_idx
+                    best_sec=sec_idx
+                    best_cyc_idx=curr_cyc
 
+    idxs2=np.copy(idxs)
+    for first_idx in idxs:
+        for sec_idx in idxs2:
+            delta=get_delta(dist_m,(first_idx,sec_idx),cyc1,cyc2)
+            if delta<best_delta:
+                best_method=OUT
+                best_delta=delta
+                best_first=first_idx
+                best_sec=sec_idx
+                #cyc1[first_idx],cyc2[sec_idx]=cyc2[sec_idx],cyc1[first_idx]
+    if best_delta<0:
 
-def get_best_edges_swap(cycle, distance_matrix):
-    best_cycle_with_swap = None
-    best_swap_length = np.inf
-    for idx_1 in range(len(cycle)):
-        if idx_1 - 1 < 0:
-            edge_1 = (-1, idx_1)
-        else:
-            edge_1 = (idx_1 - 1, idx_1)
-        for idx_2 in range(len(cycle)):
-            if idx_2 - 1 < 0:
-                edge_2 = (-1, idx_2)
-            else:
-                edge_2 = (idx_2 - 1, idx_2)
-            if edge_1 != edge_2 and edge_1[1] != edge_2[0]:
-                cycle_with_swap = copy.deepcopy(cycle)
-                cycle_with_swap[edge_1[0]+1:edge_2[0]+1] = cycle_with_swap[edge_1[0]+1:edge_2[0]+1][::-1]
-                cycle_with_swap_length = cycle_length(cycle_with_swap, distance_matrix)
-                if best_swap_length - cycle_with_swap_length > 0:
-                    best_swap_length = cycle_with_swap_length
-                    best_cycle_with_swap = cycle_with_swap
-    return best_swap_length, best_cycle_with_swap
+        improved=True
+        if best_method==IN:
+            cycs[best_cyc_idx]=make_improvement(dist_m,best_first,best_sec,cycs[best_cyc_idx])
+            cyc1,cyc2=cycs
+        elif best_method==OUT:
+            cyc1[best_first],cyc2[best_sec]=cyc2[best_sec],cyc1[best_first]
+    return best_delta,improved,cyc1,cyc2   
 
+def ls_steepest(cyc1,cyc2,dist_m):
+    imp_cycs=[np.copy(cyc1),np.copy(cyc2)]
+    improved=True
+    while improved:
+        _,improved,imp_cycs[0],imp_cycs[1]=try_to_improve(imp_cycs[0],imp_cycs[1],dist_m)
+        
+    return imp_cycs[0],imp_cycs[1]
 
-def get_best_vertices_swap(cycle, distance_matrix):
-    best_cycle_with_swap = None
-    best_swap_length = np.inf
-    for idx_1 in range(len(cycle)):
-        for idx_2 in range(idx_1, len(cycle)):
-            if idx_1 != idx_2:
-                cycle_with_swap = copy.deepcopy(cycle)
-                cycle_with_swap[idx_1], cycle_with_swap[idx_2] = cycle_with_swap[idx_2], cycle_with_swap[idx_1]
-                cycle_with_swap_length = cycle_length(cycle_with_swap, distance_matrix)
-                if best_swap_length - cycle_with_swap_length > 0:
-                    best_swap_length = cycle_with_swap_length
-                    best_cycle_with_swap = cycle_with_swap
-    return best_swap_length, best_cycle_with_swap
-
-
-def improve_cycles(cycle_1, cycle_2, distance_matrix, method):
-
-    cycle_stop = False
-    pick_cycle = 0
-
-    while not cycle_stop:
-
-        best_swap_vertices_or_edges = (None, None)
-
-        if pick_cycle % 2 == 0:
-            cycle = cycle_1
-            other_cycle = cycle_2
-        else:
-            cycle = cycle_2
-            other_cycle = cycle_1
-
-        if method == 0:
-            best_swap_vertices_or_edges = get_best_vertices_swap(cycle, distance_matrix)
-        elif method == 1:
-            best_swap_vertices_or_edges = get_best_edges_swap(cycle, distance_matrix)
-
-        best_swap_between_length, best_swap_between_cycles = \
-            get_best_vertices_swap_between_cycles(cycle_1, cycle_2, distance_matrix)
-
-        if (best_swap_vertices_or_edges[0] + cycle_length(other_cycle, distance_matrix)) - best_swap_between_length > 0:
-            best_length = best_swap_between_length
-            best_cycles = best_swap_between_cycles
-        else:
-            best_length = best_swap_vertices_or_edges[0] + cycle_length(other_cycle, distance_matrix)
-            if pick_cycle % 2 == 0:
-                best_cycles = (best_swap_vertices_or_edges[1], other_cycle)
-            else:
-                best_cycles = (other_cycle, best_swap_vertices_or_edges[1])
-
-        if (cycle_length(cycle_1, distance_matrix) + cycle_length(cycle_2, distance_matrix)) - best_length > 0:
-            cycle_1, cycle_2 = best_cycles
-        else:
-            cycle_stop = True
-
-        pick_cycle += 1
-
-    return cycle_1, cycle_2
-
-
-def get_best_vertices_swap_between_cycles(cycle_1, cycle_2, distance_matrix):
-    best_cycles_with_swap = (None, None)
-    best_swap_length_sum = np.inf
-    for idx_1 in range(len(cycle_1)):
-        for idx_2 in range(len(cycle_2)):
-            cycle_1_with_swap = copy.deepcopy(cycle_1)
-            cycle_2_with_swap = copy.deepcopy(cycle_2)
-            cycle_1_with_swap[idx_1], cycle_2_with_swap[idx_2] = cycle_2_with_swap[idx_2], cycle_1_with_swap[idx_1]
-            cycle_1_with_swap_length = cycle_length(cycle_1_with_swap, distance_matrix)
-            cycle_2_with_swap_length = cycle_length(cycle_2_with_swap, distance_matrix)
-            if best_swap_length_sum - (cycle_1_with_swap_length + cycle_2_with_swap_length) > 0:
-                best_swap_length_sum = cycle_1_with_swap_length + cycle_2_with_swap_length
-                best_cycles_with_swap = (cycle_1_with_swap, cycle_2_with_swap)
-    return best_swap_length_sum, best_cycles_with_swap
-
-
-def steepest_local_search(cycle_1, cycle_2, distance_matrix, method):
-    """
-
-    :param cycle_1_initial:
-    :param cycle_2_initial:
-    :param distance_matrix:
-    :param method: 1 -> exchanges of edges within cycles and exchanges of verices between cycles,
-    0 -> exchanges of verices within cycles and exchanges of verices between cycles
-    :return:
-    """
-
-    cycle_1, cycle_2 = improve_cycles(cycle_1, cycle_2, distance_matrix, method)
-
-    return cycle_1, cycle_2
-
-
-# coordinates_a = load_data_from_file('../Heuristics/kroA100.tsp')
-# distance_matrix_a = create_distance_matrix('../Heuristics/kroA100.tsp')
-# cycle_1, cycle_2 = greedy_cycle(distance_matrix_a, start_vertex_1=None)
-# print(cycle_length(cycle_1, distance_matrix_a))
-# print(cycle_length(cycle_2, distance_matrix_a))
-# xs = [coord[0] for coord in coordinates_a]
-# ys = [coord[1] for coord in coordinates_a]
-# plot_results(xs, ys, cycle_1, cycle_2)
-# cycle_1_improved, cycle_2_improved = steepest_local_search(cycle_1, cycle_2, distance_matrix_a, method=0)
-# print(cycle_length(cycle_1_improved, distance_matrix_a))
-# print(cycle_length(cycle_2_improved, distance_matrix_a))
-# plot_results(xs, ys, cycle_1_improved, cycle_2_improved)
