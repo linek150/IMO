7f7f159566ea2df139fced9b9b1ef489bcbe448a
JuliaMaria <j.may2501@gmail.com>
2021-05-22 16:38:05 +0200

    perform_ls parameter added

--- GeneticSearch/GeneticSearch.py
+++ GeneticSearch/GeneticSearch.py
@@ -47,8 +47,11 @@ class GeneticSearch:
         initial_population = []
         for i in range(self.elite_population):
             cycle_1, cycle_2 = self.get_random_cycles()
+            cycle_1, cycle_2 = ls_steepest_list(cycle_1, cycle_2, self.distance_matrix)
             fitness = self.calculate_fitness(cycle_1, cycle_2)
-            initial_population.append(Individual(cycle_1, cycle_2, fitness))
+            individual = Individual(cycle_1, cycle_2, fitness)
+            if individual not in initial_population:
+                initial_population.append(individual)
         return initial_population
 
     def pair_grouper(self, iterable, n, fillvalue=None):
@@ -71,8 +74,7 @@ class GeneticSearch:
             GeneticSearch.remove_edges_from_cyc(edges_to_rm,cycle)
         child.recreate_cycles(self.distance_matrix)
         return child
-        
-            
+
     @staticmethod
     def remove_edges_from_cyc(edges,cycle):
         prev_edge=edges[0]
@@ -85,6 +87,7 @@ class GeneticSearch:
             cycle[idx]=elem
         while len(cyc_copy)<len(cycle):
             cycle.pop()
+
     @staticmethod
     def occur_in_individual(edge,individual):
         for cycle in individual.cycles:
@@ -94,25 +97,28 @@ class GeneticSearch:
                     return True
         return False
 
-    def search(self):
+    def search(self, perform_ls):
         start_time = time.time()
         while time.time() - start_time < self.runtime:
             np.random.shuffle(self.population_list)
             all_individuals_pairs = [pair for pair in self.pair_grouper(self.population_list, 2)]
             for parent_1, parent_2 in all_individuals_pairs:
                 child = self.recombination(parent_1, parent_2)
-                child.cycle_1, child.cycle_2 = ls_steepest_list(child.cycle_1, child.cycle_2, self.distance_matrix)
+                if perform_ls:
+                    child.cycle_1, child.cycle_2 = ls_steepest_list(child.cycle_1, child.cycle_2, self.distance_matrix)
                 child.fitness = self.calculate_fitness(child.cycle_1, child.cycle_2)
                 if child not in self.population_list:
                     self.population_list.append(child)
             population_list_sorted = sorted(self.population_list, key=lambda x: x.fitness, reverse=True)
             best_individuals = population_list_sorted[:self.elite_population]
             self.population_list = best_individuals
-        best=max(self.population_list, key=attrgetter('fitness'))
-        return best.cycle_1,best.cycle_2
-def genetic_search(distance_matrix, runtime=5):
-    gen_search_obj=GeneticSearch(distance_matrix,runtime)
-    return gen_search_obj.search()
+        best = max(self.population_list, key=attrgetter('fitness'))
+        return best.cycle_1, best.cycle_2
+
+
+def genetic_search(distance_matrix, runtime=5, perform_ls=True):
+    gen_search_obj = GeneticSearch(distance_matrix, runtime)
+    return gen_search_obj.search(perform_ls)
 
 
 # coordinates_a = load_data_from_file('../Heuristics/kroA100.tsp')
